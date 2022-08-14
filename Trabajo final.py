ba300b1b93f30acfc51853d9aa20b03f060dff95
Ago95fiore <ago.95fiore@gmail.com>
2022-08-14 14:12:19 -0300

    Actividad 1 consigna 5

---
+++ Trabajo final.py
@@ -0,0 +1,8 @@
+Historial= (2350, 5960, 23000, 1000, 8900)
+Puppy= Historial
+TotalGastado= 2350 + 5960+ 23000+ 1000+ 8900
+if TotalGastado < 30000:
+    print ("El total gastado es", TotalGastado)
+else: 
+    print ("Gastos por encima de lo presupuestado")
+    
