9d6a55fabd8a87bc45c8648297fb8a678298e739
Leonel <forminileonel@gmail.com>
2022-08-14 20:04:20 -0300

    agregue act 10

--- Valoración Final Python.py
+++ Valoración Final Python.py
@@ -1,3 +1,11 @@
+# 10) Crear una lista denominada “Clientes” con los nombres de los diferentes  dueños de perros.
+#
+#         Juan,  Mario,  Ariel,  Josefina,  Marianella.
+#
+#   Ordenarla alfabéticamente y mostrarla por pantalla.
+
+
+
 clientes = ["Juan",  "Mario",  "Ariel",  "Josefina",  "Marianella"]
 clientes.sort()
 
